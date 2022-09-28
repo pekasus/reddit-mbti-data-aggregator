186d9e67873cd67591a04369758c567cb38f33f5
pekasus <josh@pekasys.com>
2022-09-27 22:05:10 -0400

    get subreddit

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,28 @@ use roux::Reddit;
 fn main() {
     let me: Me = get_me();
 
+    let subreddit = Subreddit::new("rust");
+    // Now you are able to:
+
+    // Get moderators.
+    let moderators = subreddit.moderators().await;
+
+    // Get hot posts with limit = 25.
+    let hot = subreddit.hot(25, None).await;
+
+    // Get rising posts with limit = 30.
+    let rising = subreddit.rising(30, None).await;
+
+    // Get top posts with limit = 10.
+    let top = subreddit.top(10, None).await;
+
+    // Get latest comments.
+    // `depth` and `limit` are optional.
+    let latest_comments = subreddit.latest_comments(None, Some(25)).await;
+
+    // Get comments from a submission.
+    let article_id = &hot.unwrap().data.children.first().unwrap().data.id.clone();
+    let article_comments = subreddit.article_comments(article_id, None, Some(25));
 }
 
 fn get_me() -> Me {
